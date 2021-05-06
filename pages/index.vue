e5a277c4113fb5e1f043eddc160e94168049ea88
Mateus Ito Silva <mateusito10@gmail.com>
2021-05-05 22:03:37 -0300

    👤 | Profile - adicionado o componente profile

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-	<div />
+	<section
+		class="flex flex-col lg:flex-row flex-1 lg:flex-none lg:mt-16 sm:gap-x-10 md:gap-x-20"
+	>
+		<div class="flex flex-col w-full lg:w-1/2">
+			<Profile />
+			<!-- Completed Challenges -->
+		</div>
+	</section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
-export default Vue.extend({});
+import Profile from "~/components/molecules/Profile.vue";
+
+interface Head {
+	title: string;
+}
+
+export default Vue.extend({
+	head: (): Head => ({
+		title: "Início | Movue It Nuxt"
+	}),
+
+	components: {
+		Profile
+	}
+});
 </script>
