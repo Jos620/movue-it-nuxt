22e5addb6eb47c43701c629811fb81d903570546
Mateus Ito Silva <mateusito10@gmail.com>
2021-05-06 12:34:54 -0300

    ⏱ | Countdown - adicionado o componente de countdown

---
+++ components/atoms/CountdownDigit.vue
@@ -0,0 +1,31 @@
+<template>
+	<p class="w-1/2 flex gap-x-1">
+		<span
+			v-for="(digit, i) in splittedDigits"
+			:key="i"
+			class="w-1/2 bg-white rounded-md flex justify-center items-center py-3"
+		>
+			{{ digit }}
+		</span>
+	</p>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import { splitValue } from "~/utils";
+
+export default Vue.extend({
+	name: "CountdownDigit",
+
+	props: {
+		digits: { type: Number, required: true }
+	},
+
+	computed: {
+		splittedDigits(): string[] {
+			return splitValue(this.digits);
+		}
+	}
+});
+</script>
--- components/molecules/Countdown.vue
+++ components/molecules/Countdown.vue
@@ -1,5 +1,63 @@
 <template>
-	<div>
-		Countdown
+	<div
+		class="flex justify-center items-center mt-8 lg:mt-14 text-9xl text-title font-rajdhani"
+	>
+		<CountdownDigit :digits="minutes" />
+		<span class="bg-background px-2">:</span>
+		<CountdownDigit :digits="seconds" />
 	</div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import { mapState, mapGetters, mapMutations } from "vuex";
+import { Mutations } from "~/store/Countdown/types";
+
+import CountdownDigit from "~/components/atoms/CountdownDigit.vue";
+
+let timeout: ReturnType<typeof setTimeout>;
+
+export default Vue.extend({
+	name: "Countdown",
+
+	components: { CountdownDigit },
+
+	computed: {
+		...mapState("Countdown", ["time", "isActive"]),
+		...mapGetters("Countdown", ["minutes", "seconds"])
+	},
+
+	methods: {
+		...mapMutations("Countdown", {
+			setTime: Mutations.SET_TIME,
+			resetTime: Mutations.RESET_TIME
+		}),
+		runCountdown(flag: boolean) {
+			if (this.isActive && flag) {
+				timeout = setTimeout(() => {
+					this.setTime(this.time - 1);
+				}, 1000);
+			} else {
+				clearTimeout(timeout);
+			}
+		}
+	},
+
+	watch: {
+		isActive(newValue: boolean) {
+			this.runCountdown(newValue);
+
+			if (!newValue) this.resetTime();
+		},
+
+		time(newValue: number) {
+			if (newValue > 0) {
+				this.runCountdown(true);
+			} else {
+				this.$emit("completed");
+			}
+		}
+	}
+});
+</script>
