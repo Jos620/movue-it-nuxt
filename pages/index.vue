5dd76213b81ed11bc037105ee5232a672a37220f
Mateus Ito Silva <mateusito10@gmail.com>
2021-05-05 23:58:50 -0300

    ⚙ | Index - adicionado os botões reponsáveis por interagir com o vuex, criado algumas utilidades

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,28 @@
 		<div class="flex flex-col w-full lg:w-1/2">
 			<Profile />
 			<CompletedChallenges />
+			<Countdown @completed="getNewChallenge" />
+			<button
+				v-if="hasCountdownCompleted"
+				class="button completed"
+				disabled
+			>
+				Cycle completed
+			</button>
+			<button
+				v-else-if="isCountdownActive"
+				class="button abandon"
+				@click="setCountdownState(false)"
+			>
+				Abandon cycle
+			</button>
+			<button
+				v-else
+				class="button start"
+				@click="setCountdownState(true)"
+			>
+				Start a cycle
+			</button>
 		</div>
 	</section>
 </template>
@@ -12,8 +34,14 @@
 <script lang="ts">
 import Vue from "vue";
 
+import { mapState, mapMutations } from "vuex";
+import { Mutations as CountdownMT } from "~/store/Countdown/types";
+
 import Profile from "~/components/molecules/Profile.vue";
 import CompletedChallenges from "~/components/atoms/CompletedChallenges.vue";
+import Countdown from "~/components/molecules/Countdown.vue";
+
+import { playAudio, sendNotification } from "~/utils";
 
 interface Head {
 	title: string;
@@ -26,7 +54,42 @@ export default Vue.extend({
 
 	components: {
 		Profile,
-		CompletedChallenges
+		CompletedChallenges,
+		Countdown
+	},
+
+	mounted() {
+		if (!("Notification" in window)) return;
+		Notification.requestPermission();
+	},
+
+	computed: {
+		...mapState("Countdown", {
+			hasCountdownCompleted: "hasCompleted",
+			isCountdownActive: "isActive"
+		})
+	},
+
+	methods: {
+		...mapMutations({
+			setCountdownHasCompleted: `Countdown/${CountdownMT.SET_HAS_COMPLETED}`,
+			setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`
+		}),
+		setCountdownState(flag: boolean) {
+			this.setCountdownHasCompleted(false);
+			this.setCountdownIsActive(flag);
+		},
+		getNewChallenge() {
+			this.setCountdownHasCompleted(true);
+
+			if (Notification?.permission === "granted") {
+				playAudio("/notification.mp3");
+				sendNotification("Novo Desafio", {
+					body: "Um novo desafio começou! Vá completá-lo!",
+					icon: "/favicon.png"
+				});
+			}
+		}
 	}
 });
 </script>
