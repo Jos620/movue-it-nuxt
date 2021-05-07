6363449764cd4ad852fd6974421fa73e7af2cea3
Mateus Ito Silva <mateusito10@gmail.com>
2021-05-07 12:30:30 -0300

    🏁 | Challenges Card, Level Up Modal - adicionado a modal de level up e o cartão de desafios

---
+++ components/atoms/StartCycle.vue
@@ -0,0 +1,18 @@
+<template functional>
+	<section
+		class="flex flex-col flex-1 justify-center items-center p-10 md:p-16"
+	>
+		<h1 class="font-medium text-center text-2xl">
+			Comece um novo ciclo para receber novos desafios
+		</h1>
+		<img
+			src="/icons/level-up.svg"
+			alt="Level Up"
+			class="mt-10 md:mt-16"
+			:style="{ minHeight: '70px' }"
+		/>
+		<p class="text-center text-md mt-4">
+			Aumente seu nível por concluir desafios
+		</p>
+	</section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -28,20 +28,28 @@
 				Start a cycle
 			</button>
 		</div>
+		<Card id="challenge" class="w-full lg:w-1/2" />
 	</section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-import { mapState, mapMutations } from "vuex";
+import { mapState, mapGetters, mapMutations } from "vuex";
+import { Mutations as ChallengesMT } from "~/store/Challenges/types";
 import { Mutations as CountdownMT } from "~/store/Countdown/types";
 
 import Profile from "~/components/molecules/Profile.vue";
 import CompletedChallenges from "~/components/atoms/CompletedChallenges.vue";
 import Countdown from "~/components/molecules/Countdown.vue";
+import Card from "~/components/organisms/Card.vue";
 
-import { playAudio, sendNotification } from "~/utils";
+import {
+	playAudio,
+	getRandomNumber,
+	scrollToElement,
+	sendNotification
+} from "~/utils";
 
 interface Head {
 	title: string;
@@ -55,7 +63,8 @@ export default Vue.extend({
 	components: {
 		Profile,
 		CompletedChallenges,
-		Countdown
+		Countdown,
+		Card
 	},
 
 	mounted() {
@@ -67,20 +76,25 @@ export default Vue.extend({
 		...mapState("Countdown", {
 			hasCountdownCompleted: "hasCompleted",
 			isCountdownActive: "isActive"
-		})
+		}),
+		...mapGetters("Challenges", ["challengesLenght"])
 	},
 
 	methods: {
 		...mapMutations({
 			setCountdownHasCompleted: `Countdown/${CountdownMT.SET_HAS_COMPLETED}`,
-			setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`
+			setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`,
+			setCurrentChallengeIndex: `Challenges/${ChallengesMT.SET_CURRENT_CHALLENGE_INDEX}`
 		}),
 		setCountdownState(flag: boolean) {
 			this.setCountdownHasCompleted(false);
 			this.setCountdownIsActive(flag);
 		},
 		getNewChallenge() {
+			const index = getRandomNumber(0, this.challengesLenght);
+
 			this.setCountdownHasCompleted(true);
+			this.setCurrentChallengeIndex(index);
 
 			if (Notification?.permission === "granted") {
 				playAudio("/notification.mp3");
@@ -89,6 +103,10 @@ export default Vue.extend({
 					icon: "/favicon.png"
 				});
 			}
+
+			this.$nextTick(() => {
+				scrollToElement("#challenges");
+			});
 		}
 	}
 });
